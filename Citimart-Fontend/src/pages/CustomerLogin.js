6e8cfd16ef7bf29db2c86a3292929c036e861a2e
25nandu <132155013+25nandu@users.noreply.github.com>
2025-08-20 17:23:05 +0530

    Update CustomerLogin.js

--- Citimart-Fontend/src/pages/CustomerLogin.js
+++ Citimart-Fontend/src/pages/CustomerLogin.js
@@ -14,7 +14,7 @@ const CustomerLogin = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://127.0.0.1:5000/auth/login/customer", {
+      const response = await fetch("https://citimart-backend.onrender.com/api/auth/login/customer", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ ...formData, role: "customer" }),
