9dda818cf422f2012d714ba19495d6d9870e5b30
25nandu <132155013+25nandu@users.noreply.github.com>
2025-08-20 16:23:43 +0530

    Update app.py

--- citimart-backend/app.py
+++ citimart-backend/app.py
@@ -7,7 +7,10 @@ load_dotenv()
 def create_app():
     app = Flask(__name__)
     CORS(app, supports_credentials=True, origins=["http://localhost:3000"], methods=["GET", "POST", "OPTIONS", "PUT", "DELETE"])
-    
+    @app.route("/")
+    def home():
+    return {"message": "Citimart Backend Running 🚀"}
+
    
     from routes.auth_routes import auth_bp
     from routes.customer_routes import customer_bp
